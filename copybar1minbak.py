e956a35aae611e6fe665f3d04cef5abe81995e25
lamter <lamter.fu@gmail.com>
2017-10-26 09:36:05 +0800

    - add : 增加将异地bar1min_bak 备份到本地的操作

---
+++ copybar1minbak.py
@@ -0,0 +1,103 @@
+"""
+从 ctp.bar1min_bak 复制数据到本地
+"""
+import datetime
+import pymongo
+import json
+import arrow
+import pytz
+from bson.codec_options import CodecOptions
+import time
+
+LOCAL_TIMEZONE = pytz.timezone('Asia/Shanghai')
+
+
+class Mover(object):
+    """
+
+    """
+
+    def __init__(self, mongoConf, startDate):
+        self.mongoConf = mongoConf
+        if "+08:00" not in startDate:
+            raise ValueError('没有指定时区 +08:00')
+        self.startDate = arrow.get(startDate).datetime
+        now = datetime.datetime.now()
+        self.endDate = arrow.get(now.strftime('%Y-%m-%d 00:00:00+08:00'))
+
+        # 本地collection
+        self.localCol = None
+        # 远程 collection
+        self.remoteCol = None
+
+    def dbConnect(self):
+        self.localCol = self._initMongo(self.mongoConf['localMongo'])
+        self.remoteCol = self._initMongo(self.mongoConf['remoteMongo'])
+
+    def _initMongo(self, conf):
+        client = pymongo.MongoClient(
+            host=conf['host'],
+            port=conf['port']
+        )
+        db = client[conf['dbn']]
+
+        # 登陆
+        if conf.get('username'):
+            db.authenticate(conf['username'], conf['password'])
+
+        return db[conf['collection']].with_options(
+            codec_options=CodecOptions(tz_aware=True, tzinfo=LOCAL_TIMEZONE))
+
+    def init(self):
+        self.dbConnect()
+
+    def start(self):
+        dt = self.startDate
+        while dt <= self.endDate:
+            print(dt)
+            sql = {
+                'tradingDay': dt,
+            }
+            cursor = self.remoteCol.find(sql, {'_id': 0})
+            count = 0
+            documents = []
+            amount = 0
+            for d in cursor:
+                count += 1
+                amount += 1
+                documents.append(d)
+                if count > 1000:
+                    print(amount)
+                    self.localCol.insert_many(documents)
+                    count = 0
+                    documents = []
+                    time.sleep(1)
+            if documents:
+                print(amount)
+                self.localCol.insert_many(documents)
+                time.sleep(1)
+
+
+            dt += datetime.timedelta(days=1)
+
+if __name__ == '__main__':
+    settingFile = 'conf/copy.json'
+    loggingConfigFile = 'conf/logconfig.json'
+    # serverChanFile = 'conf/serverChan.json'
+
+    if __debug__:
+        settingFile = 'tmp/copy.json'
+        loggingConfigFile = 'tmp/logconfig.json'
+
+    # with open(serverChanFile, 'r') as f:
+    #     serverChanUrls = json.load(f)['serverChanSlaveUrls']
+
+    with open(settingFile, 'r') as f:
+        kwargs = json.load(f)
+
+    with open(loggingConfigFile, 'r') as f:
+        logConfig = json.load(f)
+
+    mainEngine = Mover(**kwargs)
+    mainEngine.init()
+    mainEngine.start()
