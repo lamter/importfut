fb9ac646a35722614efe2df0efc6c5000395a34e
lamter <lamter.fu@gmail.com>
2017-11-02 16:03:47 +0800

    - change : 更改起末时间

--- copybar1minbak.py
+++ copybar1minbak.py
@@ -100,7 +100,7 @@ if __name__ == '__main__':
         logConfig = json.load(f)
 
     startDate = arrow.get('2017-11-01 00:00:00+08:00').datetime
-    # endDate = arrow.get('2017-08-31 00:00:00+08:00').datetime
+    endDate = arrow.get('2011-01-01 00:00:00+08:00').datetime
     endDate = None
     mainEngine = Mover(startDate=startDate, endDate=endDate,**kwargs)
     mainEngine.init()
